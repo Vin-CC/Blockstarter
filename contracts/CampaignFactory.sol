16a7ac5328b472d6f1b079c1ca86a2372a99108b
Vin-CC <vincent.cance@gmail.com>
2022-08-24 09:29:18 +0200

    update campaignfactory contract

--- contracts/CampaignFactory.sol
+++ contracts/CampaignFactory.sol
@@ -6,7 +6,7 @@ contract CampaignFactory {
     address[] private campaigns;
 
     function createCampaign(uint _minimum, string memory _name, string memory _symbol) public returns(bool) {
-        address newCampaign = address(new Campaign(_minimum, _name, _symbol));
+        address newCampaign = address(new Campaign(_minimum, _name, _symbol, msg.sender));
         campaigns.push(newCampaign);
 
         return true;
