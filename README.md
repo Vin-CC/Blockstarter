f3dad5dcfc9785cd0a1db450fed7c1b379ea79ab
Vin-CC <vincent.cance@gmail.com>
2022-08-24 02:12:07 +0200

    add campaign page + update scipt

--- README.md
+++ README.md
@@ -15,3 +15,20 @@ Pour lancer le serveur local
 ```
 npm run dev
 ```
+
+# Objectifs
+- afficher la liste des campagnes en cours
+- permettre de créer de nouvelles campagnes
+- afficher les informations d'une campagne
+- participer à une campagne
+- faire une demande de dépense
+- afficher la liste des demande de dépense
+- accepter une demande de dépense
+- finaliser une demande de dépense
+- cacher les boutons et les informations en fonction du type d'utilisateur (créateur de campagne, participant, aucun des deux)
+
+## Bonus
+- mettre objectif de financement et mettre la campagne en tant que terminée
+- mettre à jour les informations de la page courante lorsqu'une transaction a été soumise
+- enregister une image sur la blockchain et l'afficher (banière d'une campagne)
+- améliorer l'UI
--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import CampaignFactoryJSON from "./contracts/CampaignFactory.json";
 
 import state from "./state";
 
+/**
+ * CONSTANT
+ */
+
 const mainBlock = document.getElementById("main");
 const connectButton =  document.getElementById("connect-button");
-const connectedStatusLabel = document.getElementById("connected-status");
+const addressP = document.getElementById("address");
 
 const CAMPAIGN_FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
@@ -15,10 +19,24 @@ const ABI = {
     FACTORY: CampaignFactoryJSON.abi
 }
 
+/**
+ * FUNCTIONS
+ */
+
 const getProvider = async () => {
     state.provider = new ethers.providers.Web3Provider(window.ethereum)
 }
 
+const getSigner = async () => {
+    state.signer = state.provider.getSigner();
+}
+
+// Get an instance of the CampaignFactory contract
+const getCampaignFactoryInstance = async () => {
+    return new ethers.Contract(CAMPAIGN_FACTORY_ADDRESS, ABI.FACTORY, state.provider);
+}
+
+// Check if the dApp is connected to MetaMask
 const isMetaMaskConnected = async () => {
     try {
         const accounts = await state.provider.listAccounts();
@@ -28,19 +46,43 @@ const isMetaMaskConnected = async () => {
     }
 }
 
-const connectToContract = async () => {
+// Get the list of the deployes Campaigns
+const getDeployedCampaigns = async () => {
     try {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        
-        const campaignFactory = new ethers.Contract(CAMPAIGN_FACTORY_ADDRESS, ABI.FACTORY, signer);
-
-        const deployedCampaigns = await campaignFactory.getDeployedCampaigns();
-    } catch (err) {
-        console.error("connectToContract error", err);
+        const campaignFactory = await getCampaignFactoryInstance();
+        return await campaignFactory.getDeployedCampaigns();
+    } catch (error) {
+        console.error("getDeployedCampaigns error", error);
     }
 }
 
+const showDeployedCampaigns = async () => {
+    const deployedCampaigns = await getDeployedCampaigns();
+    let nbCampaignsElems = document.getElementsByName("nb-campaigns");
+    nbCampaignsElems.forEach(elem => elem.innerText = deployedCampaigns.length)
+}
+
+// Update state and UI depending of the connection to MetaMask
+const updateConnectState = async () => {
+    state.isConnected = await isMetaMaskConnected()
+    let connectedOnlyElems = document.getElementsByName("connected-only");
+    let disconnectedOnlyElems = document.getElementsByName("disconnected-only");
+
+    if (state.isConnected) {
+        getSigner()
+        const currentAddress = await state.signer.getAddress()
+
+        connectedOnlyElems.forEach(elem => elem.hidden = false)
+        disconnectedOnlyElems.forEach(elem => elem.hidden = true)
+        addressP.innerText = `Connecté : ${currentAddress}`
+    } else {
+        connectedOnlyElems.forEach(doc => doc.hidden = true)
+        disconnectedOnlyElems.forEach(doc => doc.hidden = false)
+        connectButton.innerText = "Connexion"
+    }
+}
+
+// Function that connect the dApp to MetaMask
 window.connect = async function connect() {
     if (!state.isConnected) {
         // A Web3Provider wraps a standard Web3 provider, which is
@@ -53,27 +95,26 @@ window.connect = async function connect() {
         // The MetaMask plugin also allows signing transactions to
         // send ether and pay to change state within the blockchain.
         // For this, you need the account signer...
-        state.signer = state.provider.getSigner();
-
-        await connectToContract();
+        getSigner();
+        console.log("state.signer", state.signer);
     }
 }
 
+// Event accountsChanged throw when we disconnect the wallet from the dApp
+window.ethereum.on("accountsChanged", (accounts) => {
+    console.log("accountsChanged");
+    updateConnectState()
+})
+
+// Init function
 async function init() {
+    //check if there is MetaMask installed on the browser
     getProvider()
     if (state.provider) {
-        state.isConnected = await isMetaMaskConnected()
-        
-        if (state.isConnected) {
-            connectButton.innerText = "se déconnecter"
-
-            await connectToContract()
-        } else {
-            connectButton.innerText = "se connecter"
-        }
-        connectedStatusLabel.innerText = state.isConnected
+        await updateConnectState()
+        await showDeployedCampaigns()
     } else {
-        mainBlock.innerHTML = "<h1>Metamask n'est pas présent, installez le!</h1>"
+        mainBlock.innerHTML = "<h1>MetaMask n'est pas présent, installez le!</h1>"
     }
 }
 
